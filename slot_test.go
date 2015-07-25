235bc9775416f7ac42e2b564c8ed64b06ceca16e
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-07-25 11:07:51 +0900

    Update test

--- slot_test.go
+++ slot_test.go
@@ -3,21 +3,31 @@ package slot
 import (
 	"bytes"
 	"reflect"
+	"strings"
 	"testing"
 )
 
 func TestSlotStart(t *testing.T) {
-	w := bytes.NewBuffer([]byte{})
-	expected := []string{"h", "o", "g", "e"}
-	s := New(expected, w)
-	s.Start()
+	assert := func(expected []string) {
+		w := bytes.NewBuffer([]byte{})
+		s := New(expected, w)
+		s.Start()
 
-	b := w.Bytes()
-	var got []string
-	for _, v := range b[len(b)-4:] {
-		got = append(got, string(v))
+		b := w.Bytes()
+		var got []string
+		for _, v := range b[len(b)-4:] {
+			got = append(got, string(v))
+		}
+		if !reflect.DeepEqual(got, expected) {
+			t.Fatalf("Output should be end with %v, but got %v", expected, got)
+		}
+
+		if c := strings.Count(string(b), strings.Join(expected, "")); c != 1 {
+			t.Fatalf("Should be 1, but got %d", c)
+		}
 	}
-	if !reflect.DeepEqual(got, expected) {
-		t.Errorf("Output should be end with %v, but got %v", expected, got)
+
+	for i := 0; i < 50; i++ {
+		assert([]string{"h", "o", "g", "e"})
 	}
 }
